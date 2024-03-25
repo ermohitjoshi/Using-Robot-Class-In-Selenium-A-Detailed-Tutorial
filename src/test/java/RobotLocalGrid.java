e046ed7184bf00895d6fac85d3ba0ee7001e8412
ermohitjoshi <56245_mohitjoshi@gbpuat-tech.ac.in>
2024-03-25 19:21:08 +0530

    Update RobotLocalGrid.java

--- src/test/java/RobotLocalGrid.java
+++ src/test/java/RobotLocalGrid.java
@@ -3,7 +3,10 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.Point;
@@ -33,6 +36,7 @@ public class RobotLocalGrid extends BaseTest {
     Robot robot;
     int xcord;
     int ycord;
+    
 
     @BeforeClass
     public void navigateToWebsite() throws AWTException, InterruptedException
@@ -51,22 +55,22 @@ public class RobotLocalGrid extends BaseTest {
         System.setProperty("java.awt.headless", "true");
     }
 
-
-
     @Test(priority = 1, description = "Test 1: Using Keyboard Keys", enabled=true)
-    public void pressing_keyboard_keys() throws InterruptedException
+    @Parameters({"testurl1"})
+    public void pressing_keyboard_keys(final String testurl1) throws InterruptedException
     {
          WebDriver driver = getDriver();
-   		 driver.get("https://ecommerce-playground.lambdatest.io/");
+   		 driver.get(testurl1);
    		 driver.findElement(By.name("search")).sendKeys("iphone");
    		 robot.keyPress(KeyEvent.VK_ENTER);
    		 robot.keyRelease(KeyEvent.VK_ENTER);
    		 robot.delay(2000);
 }
     @Test(priority = 2, description = "Test 2: Using Scrolling Page", enabled=true)
-	 public void scrollingPage() throws AWTException, InterruptedException {
+    @Parameters({"testurl1"})
+	 public void scrollingPage(final String testurl1) throws AWTException, InterruptedException {
     	WebDriver driver = getDriver();
-		 driver.get("https://ecommerce-playground.lambdatest.io/");
+		 driver.get(testurl1);
 		 Thread.sleep(2000);
 		 robot.mouseWheel(30);
 		 robot.delay(2000);
@@ -75,9 +79,10 @@ public class RobotLocalGrid extends BaseTest {
     }
 
 	 @Test(priority = 3, description = "Test 3: Using Mouse Keys", enabled=true)
-	 public void pressMouseKeys() {
+	 @Parameters({"testurl2"})
+	 public void pressMouseKeys(final String testurl2) {
 		 WebDriver driver = getDriver();
-		 driver.get("https://www.lambdatest.com/selenium-playground/checkbox-demo");
+		 driver.get(testurl2);
 		 WebElement clickButton = driver.findElement(By.id("isAgeSelected"));
 		 Point c = clickButton.getLocation();
 		 xcord = c.getX();
@@ -88,9 +93,10 @@ public class RobotLocalGrid extends BaseTest {
 	 }
     
 	 @Test(priority = 4, description = "Test 4: Downloading a File", enabled=true)
-	 public void downloadFile() throws AWTException {
+	 @Parameters({"testurl3"})
+	 public void downloadFile(final String testurl3) throws AWTException {
 		 WebDriver driver = getDriver();
-		 driver.get("https://www.lambdatest.com/selenium-playground/download-file-demo");
+		 driver.get(testurl3);
 		 robot.delay(2000);
 		 driver.findElement(By.linkText("Download File")).click();
 		 robot.delay(5000);
@@ -99,9 +105,10 @@ public class RobotLocalGrid extends BaseTest {
 	 }
 	 
 	 @Test(priority =5, description = "Test 5: Uploading a File", enabled=true)
-	 public void uploadFile() throws AWTException{
+	 @Parameters({"testurl4"})
+	 public void uploadFile(final String testurl4) throws AWTException{
 		 WebDriver driver = getDriver();
-		 driver.get("https://www.lambdatest.com/selenium-playground/upload-file-demo");	 
+		 driver.get(testurl4);	 
 	 	 robot.delay(2000);
 	 	// Path of the file to be uploaded 
 			StringSelection file = new StringSelection("C:\\Users\\mohit\\OneDrive\\Desktop\\demofile.png");
@@ -134,17 +141,19 @@ public class RobotLocalGrid extends BaseTest {
 	 }
 	 
 	 @Test (priority = 6, description = "Test 5: Taking a Screenshot", enabled=true)
-	 public void takingScreenshot() throws IOException, HeadlessException, AWTException {
+	 @Parameters({"testurl5"})
+	 public void takingScreenshot(final String testurl5) throws IOException, HeadlessException, AWTException {
 		 WebDriver driver = getDriver();
-		 driver.get("https://www.lambdatest.com/selenium-playground/virtual-dom");	
+		 driver.get(testurl5);	
 			
 			// Command to Capture Screenshot 
 			BufferedImage capturedScreenshot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
 			
-			//Creating destination for the output file 
-			  File destination = new File("C:\\Users\\mohit\\eclipse-workspace\\usingSeleniumRobotClass\\Screenshot\\currentScreenshot.jpg");
+			//Creating a destination for the output file 
+			  File destination = new File("C:\\Users\\mohit\\eclipse-workspace\\using-robot-class-in-selenium\\Screenshots\\currentScreenshot.jpg");
 			  
-			// This will store screenshot on Specific location with a specific extension of image
+			// This will store screenshots on Specific locations with a specific extension of the image
 			ImageIO.write(capturedScreenshot, "jpg", destination);
 	 }
-    }
+	 
+ }
